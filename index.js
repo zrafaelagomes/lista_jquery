24dbbc2ab6f9df38706e007373aa556d3e21c05f
Rafaela Gomes <rafagiza@hotmail.com>
2023-07-12 16:51:26 -0300

    commit inicial

---
+++ index.js
@@ -0,0 +1,11 @@
+function adicionarTarefa() {
+    var tarefa = document.getElementById("tarefa").value;
+    var lista = document.getElementById("listaTarefas");
+
+if (tarefa !== "") {
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(tarefa));
+    lista.appendChild(li);
+    document.getElementById("tarefa").value = "";
+}
+}
